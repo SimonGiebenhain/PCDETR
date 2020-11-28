5ff5baa014297f9cdfa05d2b3098fd2f9b5ceeba
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2020-11-28 17:49:07 +0100

    commenting out NestedTensor

--- pcdet/models/dense_heads/detr/detr.py
+++ pcdet/models/dense_heads/detr/detr.py
@@ -36,7 +36,7 @@ class DETR(nn.Module):
         self.backbone = backbone
         self.aux_loss = aux_loss
 
-    def forward(self, samples: NestedTensor):
+    def forward(self, samples):
         """ The forward expects a NestedTensor, which consists of:
                - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
                - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels
