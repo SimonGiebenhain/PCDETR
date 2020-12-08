c146e4d58f030a88ea974673c2b1fa1d0386c636
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2020-12-08 14:21:20 +0100

    most simple eval possible

--- pcdet/models/detectors/pointpillar.py
+++ pcdet/models/detectors/pointpillar.py
@@ -18,8 +18,9 @@ class PointPillar(Detector3DTemplate):
             }
             return ret_dict, tb_dict, disp_dict
         else:
-            pred_dicts, recall_dicts = self.post_processing(batch_dict)
-            return pred_dicts, recall_dicts
+            return batch_dict
+            #pred_dicts, recall_dicts = self.post_processing(batch_dict)
+            #return pred_dicts, recall_dicts
 
     def get_training_loss(self):
         disp_dict = {}
